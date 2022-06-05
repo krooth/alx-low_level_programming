51c3a87ab66d3ed1478b963aec552689bfd4abb1
krooth <hermon.kidanew@gmail.com>
2022-06-05 09:11:07 +0000

    low-level programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+   * main - entry point
+   * void - doesn't any
+   *
+   * Return: returns 0 when program finsishes excuting
+   */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
