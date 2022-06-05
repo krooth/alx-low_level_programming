358f297e5c9b2c9a422be187d4249126d2564184
krooth <hermon.kidanew@gmail.com>
2022-06-05 19:12:00 +0000

    low-level programming variables, if-else, while

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+/**
+   * main - entry point
+   * void - no parameter for main
+   *
+   * Return: returns 0 when program finishes excution
+   */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n % 10 > 5)
+	{
+		printf("%s\s%d\s%s\s%d\s%s\n", "Last digit of ", n, " is ", n%10,
+		"and is greater than 5");
+	}
+	if (n % 10 == 0)
+	{
+		printf("%s %d %s %d %s\n", "Last digit of ", n, " is ", n%10, 
+				"and is 0");
+	}
+	if (n % 10 > 0 && n % 10 < 5) 
+	{
+		printf("%s %d %s %d %s\n", "Last digit of ", n, " is ", n%10,
+				"and is 0");
+	}
+	return (0);
+}
