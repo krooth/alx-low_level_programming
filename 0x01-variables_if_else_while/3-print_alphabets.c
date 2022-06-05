36a3e351c4d7fd4ba814df9c0e355b8976cac0f5
krooth <hermon.kidanew@gmail.com>
2022-06-05 19:56:53 +0000

    low-level programming variables, if-else, while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+   * main - entry point
+   * void - no parameter for main
+   *
+   * Return: returns 0 when program finishes excution
+   */
+
+int main(void)
+{
+	char alphabet_char = 97;
+
+	while (alphabet_char < 123)
+	{
+		putchar(alphabet_char);
+		alphabet_char++;
+	}
+	alphabet_char = 65;
+	while (alphabet_char < 91)
+	{
+		putchar(alphabet_char);
+		alphabet_char++;
+	}
+	putchar("\n");
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -2,3 +2,4 @@ This is part two of the low-level programming with C
 0-positive_or_negarive.c: is program that tells if the random number generated is negative, positive or zero.
 1-last_digit.c: is program that identifeis the last digit and tells if it's zero, greater than 5 or in between.
 2-print_alphabet.c: prints alphabet in lowercase.
+3-print_aphabets.c: prints alphabets in lowercase and uppercase.
