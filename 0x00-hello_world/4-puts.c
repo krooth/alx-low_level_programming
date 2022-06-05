f7b8c82c5f04d468e2050ae5d6823e12d4316372
krooth <hermon.kidanew@gmail.com>
2022-06-05 09:03:33 +0000

    low-level programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <string.h>
-
+/**
+  * main - Entry point
+  * void - doesn't not any
+  *
+  * Return: 0 when the program finishes excuting
+  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
