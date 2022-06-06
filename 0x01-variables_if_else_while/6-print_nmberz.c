9e49b913e9f9bfd350cff0c77953cb5be200894b
krooth <hermon.kidanew@gmail.com>
2022-06-06 19:37:22 +0000

    low-level programming

--- 0x01-variables_if_else_while/6-print_nmberz.c
+++ 0x01-variables_if_else_while/6-print_nmberz.c
@@ -10,11 +10,13 @@
 
 int main(void)
 {
-	int num =0;
+	int num = 48;
 
-	while (num < 10)
+	while (num < 58)
 	{
 		putchar(num);
+		num++;
 	}
+	putchar('\n');
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+/**
+   * main - entry point
+   * void - no parameter for main
+   *
+   * Return: returns 0 when program finishes excution
+   */
+
+int main(void)
+{
+		int num = 48;
+
+		while (num < 58)
+		{
+				putchar(num);
+				num++;
+		}
+		num = 97;
+		while (num < 114)
+		{
+				putchar(num);
+				num++;
+		}
+		putchar('\n');
+		return (0);
+}
+
