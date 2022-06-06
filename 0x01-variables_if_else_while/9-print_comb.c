05bcb17687c5bfcacce391c9b699deb399a9f3ac
krooth <hermon.kidanew@gmail.com>
2022-06-06 22:58:22 +0300

    low-level programming

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+   * main - entry point
+   * void - no parameter for main
+   *
+   * Return: returns 0 when program finishes excution
+   */
+
+int main(void)
+{
+	int n = 0;
+	
+	while (n < 10)
+	{
+		putchar('0' + n);
+		if (n != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
