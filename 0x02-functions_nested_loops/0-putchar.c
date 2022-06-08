187ecb98d71d311de1e6700ba5189711b039e964
krooth <hermon.kidanew@gmail.com>
2022-06-08 04:43:15 +0300

    low-level programming

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/**
+   * main - entry point
+   * void - no parameter for main
+   *
+   * Return: returns 0 when program finishes excution
+   */
+
+int main(void)
+{
+	printf("%s\n", "_putchar");
+
+	return (0);
+}
