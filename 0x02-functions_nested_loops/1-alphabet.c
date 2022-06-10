bbd8f501a9fffbdda5e594555df4feaf22d42f4e
krooth <hermon.kidanew@gmail.com>
2022-06-10 16:44:04 +0300

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+        * main - entry point
+        * void - no parameter for main
+        *
+        * Return: returns 0 when program finishes excution
+        */
+void
+
+
+/**
+        * main - entry point
+        * void - no parameter for main
+        *
+        * Return: returns 0 when program finishes excution
+        */
+int main(void)
+{
+
+}
