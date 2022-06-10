2e5391c150b697947a28480b40c47be751d79e1a
krooth <hermon.kidanew@gmail.com>
2022-06-10 17:00:37 +0300

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
-        * main - entry point
-        * void - no parameter for main
-        *
-        * Return: returns 0 when program finishes excution
-        */
-void
-
-
-/**
-        * main - entry point
-        * void - no parameter for main
-        *
-        * Return: returns 0 when program finishes excution
-        */
-int main(void)
+		* print_alphabet - function
+		* void - no parameter for print_alphabet
+		*
+		* Return: returns 0 when program finishes excution
+		*/
+void print_alphabet(void)
 {
+	char letter = 97;
 
+	while (letter < 123)
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+	return (0);
 }
