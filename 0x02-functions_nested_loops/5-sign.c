30bbf87553beae388c302f23d9ed67d99ac28250
krooth <hermon.kidanew@gmail.com>
2022-06-11 10:52:11 +0300

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+	* print_sign - checks if character is alphabetic
+	* @n: integer parameter for print_sign()
+	*
+	* Return: returns 1 if character is alphabetic and 0 if not
+	*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+}
