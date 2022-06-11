93ea72deb2e36326989f2e828ff884e6e767f638
krooth <hermon.kidanew@gmail.com>
2022-06-11 17:57:42 +0300

    more functions nested loops task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+	* more_numbers - print number 0 to 9, 10 times
+	* void: no parameters
+	*
+	* Return: returns 0 when program finishes excution
+	*/
+
+
+void more_numbers(void)
+{
+	int i = 0, j = 0;
+
+	while (i < 10)
+	{
+		while (j < 15)
+		{
+			_putchar('0' + j);
+			j++;
+		}
+		i++;
+	}
+}
+
