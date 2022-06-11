6e0d5ddda9719e990fc59753c65df3042dde6fb7
krooth <hermon.kidanew@gmail.com>
2022-06-11 17:48:44 +0300

    more functions nested loops task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+	* print_most_numbers - print number 0 to 9 except 2 and 4
+	* void: no parameters
+	*
+	* Return: returns 0 when program finishes excution
+	*/
+
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		else
+			_putchar('0' + i);
+	}
+	_putchar('\n');
+}
