4e5eccdd4f2d9fe6207adb5e6ef2462c42f590ba
krooth <hermon.kidanew@gmail.com>
2022-06-11 17:42:36 +0300

    more functions nested loops task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+	* print_numbers - returns the product of two ints
+	* void: non void
+	*
+	* Return: returns 0 when program finishes excution
+	*/
+
+void print_numbers(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		_putchar(n);
+		n++;
+	}
+}
