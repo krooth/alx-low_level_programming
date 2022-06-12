7110bb8769a101372d1dfbaf6ca83ff824420f1c
krooth <hermon.kidanew@gmail.com>
2022-06-12 20:32:31 +0300

    task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+	* print_square - prints line
+	* @size: integer parameter
+	*
+	* Return: returns 0 when program finishes excution
+	*/
+
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
