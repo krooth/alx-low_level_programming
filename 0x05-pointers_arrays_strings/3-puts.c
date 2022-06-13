07e05afc79621d00c4a87fe5f8ff729fd9b04e9f
krooth <hermon.kidanew@gmail.com>
2022-06-13 21:30:37 +0300

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+	* _puts - prints characters to stdout
+	* @str: character pointer parameter
+	*
+	* Return: returns nothing
+	*/
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+}
