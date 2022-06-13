762d384ebd4757bf4db5ef28a10ff309df78085b
krooth <hermon.kidanew@gmail.com>
2022-06-13 21:09:36 +0300

    pointers and arrays task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+	* reset_to_98 - resets n to 98
+	* @n: pointer integer parameter
+	*
+	* Return: doesn't have return type
+	*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
