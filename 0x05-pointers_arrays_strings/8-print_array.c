3f1518462763c0c567c1b4395d67a558fb79e252
krooth <hermon.kidanew@gmail.com>
2022-06-13 22:32:07 +0300

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.c"
+
+/**
+	* print_array - prints array
+	* @a: integer array
+	* @n: integer
+	*
+	* Return: returns nothing
+	*/
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (int i = 0; i < n; i++)
+	{
+		if (a[i] != '\0')
+			printf("%d, ", a[i]);
+	}
+	printf("\n");
+}
