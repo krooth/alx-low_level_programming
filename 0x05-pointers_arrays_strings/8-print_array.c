ddb4f2a375a548723d751a6b5542d28d8413fbca
krooth <hermon.kidanew@gmail.com>
2022-06-13 22:44:04 +0300

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -21,11 +21,10 @@ void print_array(int *a, int n)
 	{
 		for (i = 0; i < n; i++)
 		{
-			if (a[i] < n - 1)
+			if (i < n - 1)
 				printf("%d, ", a[i]);
 			else
 				printf("%d\n", a[i]);
 		}
 	}
-	printf("\n");
 }
