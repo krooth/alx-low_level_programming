0b77905d49dcff27d43d6f84c3d2147ef27a3185
krooth <hermon.kidanew@gmail.com>
2022-06-16 22:58:34 +0300

    task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -14,13 +14,13 @@ void reverse_array(int *a, int n)
 	int i, j;
 	int temp;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n - 1; i++)
 	{
 		for (j = n - 1; j >= i; j--)
 		{
 			temp = a[j - 1];
 			a[j - 1] =  a[j];
 			a[j] = temp;
-		}
+		}	
 	}
 }
