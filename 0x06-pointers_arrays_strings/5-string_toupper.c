410a056121a72488d491c8953c5bd4788787ebb7
krooth <hermon.kidanew@gmail.com>
2022-06-16 23:13:05 +0300

    task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
-	* reverse_array - concatnates strings
-	* @a: int parameter
-	* @n: int parameter
+	* *string_toupper - concatnates strings
+	* @str: int parameter
 	*
 	* Return: returns nothing
 	*/
@@ -11,6 +10,7 @@
 char *string_toupper(char *str)
 {
 	int i;
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] >= 97 && str[i] <= 122)
