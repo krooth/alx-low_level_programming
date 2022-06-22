0cd14ea6fd1c0bb868afa456f677df798ed2ebb5
krooth <hermon.kidanew@gmail.com>
2022-06-22 20:54:01 +0300

    task 8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+	* print_diagsums - sums diagonal of matrix
+	* @a: character array to print from
+	* @size: size of array
+	*
+	* Return: returns nothing
+	*/
+
+void set_string(char **s, char *to)
+{
+    *s = to;
+}
