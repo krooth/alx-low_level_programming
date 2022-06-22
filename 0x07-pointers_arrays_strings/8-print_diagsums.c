2f3b9bcbdd69507a2710159b1bb5deda8fb208b6
krooth <hermon.kidanew@gmail.com>
2022-06-22 20:47:12 +0300

    task 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -19,11 +19,11 @@ void print_diagsums(int *a, int size)
 		{
 			if (i == j)
 			{
-				rt_sum += *((a + i*size) +j);
+				rt_sum += *((a + i * size) + j);
 			}
 			if ((i + j) == size - 1)
 			{
-				lft_sum += *((a + i*size) +j);
+				lft_sum += *((a + i * size) + j);
 			}
 		}
 	}
