2eeb1c6205e85874bb9cfa171d1894a3b9d29290
krooth <hermon.kidanew@gmail.com>
2022-06-23 20:09:41 +0300

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,13 +10,9 @@
 void _puts_recursion(char *s)
 {
 
-	if (*s == '\0')
-	{
-		_putchar('\0');
-	}
-	else
+	if (*s != '\0')
 	{
 		_putchar(*s);
-		_puts_recursion(s+1);
+		_puts_recursion(s + 1);
 	}
 }
