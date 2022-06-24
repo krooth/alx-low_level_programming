27a74e4a4dfdda0c855771c3509367071d4a3950
krooth <hermon.kidanew@gmail.com>
2022-06-24 18:11:28 +0300

    task 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,13 +9,15 @@
 
 int factorial(int n)
 {
-	if (n == 1)
+	if (n >= 0)
 	{
-		return (1);
+		if (n == 1)
+			return (1);
+		else
+			return (n * (factorial(n - 1)));
 	}
+
 	else
-	{
-		return (n * factorial(n - 1));
-	}
+		return (-1);
 }
 
