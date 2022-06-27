0ed66a5f29fb0b4b437476be6f2d351a8484e8fc
krooth <hermon.kidanew@gmail.com>
2022-06-27 16:39:04 +0300

    task 4

--- 0x06-pointers_arrays_strings/test.c
+++ 0x06-pointers_arrays_strings/test.c
@@ -11,6 +11,7 @@ int main(void)
     char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
     char *ptr;
 
+		printf("%s", str);
     ptr = cap_string(str);
     printf("%s", ptr);
     printf("%s", str);
