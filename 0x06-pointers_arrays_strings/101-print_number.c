1cd8ec8bbf72e7c9524983283f88a17ec157e9b9
krooth <hermon.kidanew@gmail.com>
2022-06-29 10:27:37 +0300

    task 101

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,24 +1,25 @@
 #include "main.h"
 
 /**
-	* *rot13 - rotate a character by 13 for alphabets
-	* @str: char parameter
+	* print_number - prints nmbers
+	* @n: integer parameter	
 	*
-	* Return: returns character which is rotated by 13
+	* Return: returns nothing
 	*/
 
 void print_number(int n)
 {
 	int temp = n / 10, base = 1, no_of_digits = 1, i, num;
 
+	if (n == -2147483648)
+		n += 2;
 	if (n < 0)
 	{
 		n = -n;
 		_putchar('-');
 	}
 	if (n == 0)
-		no_of_digits = 1;
-	
+		no_of_digits = 1;	
 	else
 	{
 		while (temp != 0)
@@ -35,7 +36,10 @@ void print_number(int n)
 	for (i = 1; i <= no_of_digits; i++)
 	{
 		num = (temp / base) % 10;
-		_putchar('0' + num);
+		if (n == 2147483646 && i == no_of_digits)
+			_putchar('0' + 8);
+		else
+			_putchar('0' + num);
 		base /= 10;
 	}
 }
