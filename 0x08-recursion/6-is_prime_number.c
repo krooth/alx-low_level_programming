b37d9937ae7479c41af1fea30c56c9a14013e924
krooth <hermon.kidanew@gmail.com>
2022-06-30 23:04:52 +0300

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,14 +1,34 @@
 #include "stdio.h"
 
 /**
-	* _pow_recursion - returns the power of a number
-	* @x: number we are powering on
-	* @y: power
+	* _prime_num - identifies prime numbers
+	* @n: integer arguement to be checked if prime or not
+	* @i: ineteger paramenter used for checking
+	*
+	* Return: returns integer
+	*/
+
+int _prime_num(int n, int i)
+{
+	if (n <= 0 || n == 1)
+		return (0);
+	if (i == 1)
+		return (1);
+	if (n % i == 0)
+		return (0);
+	return (_prime_num(n, i - 1));
+}
+
+/**
+	* _is_prime_number - identifies prime numbers
+	* @n: integer arguement to be checked if prime or not
 	*
 	* Return: returns integer
 	*/
 
 int is_prime_number(int n)
 {
-    if (n )
+	int i = n / 2;
+
+	return (_prime_num(n, i));
 }
