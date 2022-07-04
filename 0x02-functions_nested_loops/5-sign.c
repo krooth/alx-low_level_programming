bfa95680a0bfede8062c5fae1228fd8999732627
krooth <hermon.kidanew@gmail.com>
2022-07-04 16:54:04 +0300

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,16 +12,19 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar(43);
+		_putchar('0' + n);
 		return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
+		_putchar('0' + n);
 		return (0);
 	}
 	else
 	{
 		_putchar(45);
+		_putchar('0' + n);
 		return (-1);
 	}
 }
