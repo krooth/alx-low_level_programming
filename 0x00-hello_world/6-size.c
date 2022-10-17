2a186e43a0c109f27156256397f56c8592bbece2
krooth <hermon.kidanew@gmail.com>
2022-10-17 21:26:47 +0300

    up to date

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,9 +19,9 @@ int main(void)
 	printf("%s%zu%s\n", "Size of a char: ", sizeof(char_type), " byte(s)");
 	printf("%s%zu%s\n", "Size of an int: ", sizeof(int_type), " byte(s)");
 	printf("%s%zu%s\n", "Size of a long int: ", sizeof(long_int_type),
-                        " byte(s)");
+			" byte(s)");
 	printf("%s%zu%s\n", "Size of a long long int: ", sizeof(long_long_int_type),
-                        " byte(s)");
+			" byte(s)");
 	printf("%s%zu%s\n", "Size of a float: ", sizeof(float_type), " byte(s)");
 	return (0);
 }
