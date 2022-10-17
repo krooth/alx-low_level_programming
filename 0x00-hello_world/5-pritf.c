01862924e6810a58bc40521caf09405caa3fea78
krooth <hermon.kidanew@gmail.com>
2022-10-17 21:18:52 +0300

    up to date

---
+++ 0x00-hello_world/5-pritf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  * main - entry point
+  * void - doesn't take any parameter
+  *
+  * Return: returns 0 when the program finished excuting
+  */
+
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
