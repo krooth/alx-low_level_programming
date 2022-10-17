cec3c57393d0f2f857848b5089005a62fffa074a
krooth <hermon.kidanew@gmail.com>
2022-10-17 21:36:39 +0300

    up to date

---
+++ 0x00-hello_world/101-qoute.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+  * main - entry point
+  * void - doesn't any
+  *
+  * Return: reurns 0 when the program finishes excuting.
+  */
+
+int main(void)
+{
+	fprintf(stderr,
+			"and that piece of art is userful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
