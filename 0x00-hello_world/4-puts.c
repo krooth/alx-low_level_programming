3856eab269541259f128955bee0b16630bd75f47
krooth <hermon.kidanew@gmail.com>
2022-10-17 21:14:14 +0300

    up to date

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - entry point
+  * void - doesn't take any parameter
+  *
+  * Return: returns 0 when the program finished excuting
+  */
+
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+}
