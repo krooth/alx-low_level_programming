7f68499e586689537b9ca45bfa6fbbc45efb84b1
krooth <hermon.kidanew@gmail.com>
2022-10-23 18:02:53 +0300

    C programming

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+  * main - entry point
+  * void - doesn't take any parameter
+  *
+  * Return: returns 0 when the program finished excuting
+  */
+
+int main(void)
+{
+	char i = 97;
+
+	for (i = 97; i < 123; ++i)
+	{
+		putchar(i);
+	}
+	putchar('\0');
+	return (0);
+}
