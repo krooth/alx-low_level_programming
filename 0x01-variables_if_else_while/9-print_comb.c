8ee93afa1cb801d27c51c47d897a56e8564064c7
krooth <hermon.kidanew@gmail.com>
2022-10-23 21:33:49 +0300

    C programming

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,12 +12,12 @@
 
 int main(void)
 {
-	char i = 48;
+	int i;
 
-	for (i = 48; i < 58; ++i)
+	for (i = 0; i < 10; ++i)
 	{
-		putchar(i);
-		if (i == 57)
+		putchar(i + '0');
+		if (i == 9)
 		{
 			continue;
 		}
