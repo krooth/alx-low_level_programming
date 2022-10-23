6596bb73a363c7ffc25c457d7e1aa197afb3e721
krooth <hermon.kidanew@gmail.com>
2022-10-23 21:05:03 +0300

    C programming

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+  * main - entry point
+  * void - doesn't take any parameter
+  *
+  * Return: returns 0 when the program finished excuting
+  */
+
+int main(void)
+{
+	char i = 48;
+
+	for (i = 48; i < 58; ++i)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
