cfe1c9b456b2b032fe5138351a2a7afa1f042bbe
krooth <hermon.kidanew@gmail.com>
2022-10-30 14:07:44 +0300

    C functions

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/* more headers goes there */
+
+/**
+  * print_alphabet_x10 - entry point
+  * void - doesn't take any parameter
+  *
+  * Return: does not return anything
+  */
+
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = 97; j < 123; j++)
+		{
+			_putchar(j);
+
+		}
+		_putchar('\n');
+	}
+
+}
+
