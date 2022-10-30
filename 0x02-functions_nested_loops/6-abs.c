1832b87e28514f4dda382f7bca032352987504d0
krooth <hermon.kidanew@gmail.com>
2022-10-30 15:10:34 +0300

    C functions

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - give us the absolute value of a given input
+ * @n: int parameter
+ *
+ * Return: the integer
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n *= -1;
+	}
+	return (n);
+}
