f3c90fc5c5c8f91cd2a0fb1ad51cf740d6baaf09
krooth <hermon.kidanew@gmail.com>
2022-11-07 23:10:26 +0300

    C programming

---
+++ 0x02-functions_nested_loops/9-times_tables.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * times_tables - prints the 9 times table starting with 0.
+ * void: no parameter
+ *
+ * Return: does not return
+ */
+void times_table(void)
+{
+	
+}
