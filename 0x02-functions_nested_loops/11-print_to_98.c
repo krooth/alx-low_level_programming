dd5448eaae2beb201306c80e72ed31e39d8908a7
krooth <hermon.kidanew@gmail.com>
2022-11-07 23:46:07 +0300

    C programming

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - print all natural numbers n to 98
+ * @n: integer parmeter to be print_to_98 function
+ *
+ * Return: returns integer
+ */
+int print_to_98(int n)
+{
+		
+}
