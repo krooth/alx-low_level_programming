d20fc3a2e9c72b6dc2b2a4288e5f4a56607368d1
krooth <hermon.kidanew@gmail.com>
2022-11-13 17:18:02 +0300

    C programming

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: int parameter
+ * @b: int parameter
+ *
+ * Return: product of multiplies
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
